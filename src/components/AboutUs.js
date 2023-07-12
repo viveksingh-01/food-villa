8130eb225a69600745761a0e33fd07e61b5864ba
viveksingh-01 <viveksingh9301@gmail.com>
2023-07-13 00:00:10 +0530

    wip: add AboutUs component

---
+++ src/components/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <h3>A food delivery app like Swiggy</h3>
+    </div>
+  );
+};
+
+export default AboutUs;
