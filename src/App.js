e35fc6c00c7fdc6f2ae22afc0aaf5e6099440ea6
viveksingh-01 <viveksingh9301@gmail.com>
2023-07-14 14:13:04 +0530

    feat: add react-router package to implement routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AboutUs from './components/AboutUs';
 import Header from './components/Header';
 import Home from './components/Home';
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/about',
+    element: <AboutUs />
+  }
+]);
 
 const App = () => (
   <>
     <Header />
-    <Home />
+    <RouterProvider router={appRouter} />
   </>
 );
 
