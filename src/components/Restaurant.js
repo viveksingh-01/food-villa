3d8113cbe2ca9ac65c23970250d2397375dff37d
viveksingh-01 <viveksingh9301@gmail.com>
2024-01-06 09:18:00 +0530

    wip: display add-item button beside restaurant menu items

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -29,9 +29,18 @@ export default function Restaurant() {
             itemCards.map(item => (
               <li
                 key={item.card.info.id}
-                className="w-[500px] p-2 px-3 mb-1 mr-8 bg-gray-100 rounded-md flex justify-between">
-                <span>{item.card.info.name}</span>{' '}
-                <span>Rs. {item.card.info.price / 100 || item.card.info.defaultPrice / 100}</span>
+                className="w-[360px] p-2 px-3 mb-1 mr-8 bg-gray-100 rounded-md flex justify-between items-center">
+                <div className="flex flex-col">
+                  <span className="text-gray">{item.card.info.name}</span>{' '}
+                  <span className="font-semibold">
+                    Rs. {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+                  </span>
+                </div>
+                <div>
+                  <button className="p-2 px-4 text-sm border-2 border-green-500 rounded-md bg-green-500 text-white hover:bg-green-600 hover:border-green-600">
+                    Add
+                  </button>
+                </div>
               </li>
             ))
           )}
