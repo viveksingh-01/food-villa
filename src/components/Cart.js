fb7291dd6fc514a7039f8cd024b895fea846f2db
viveksingh-01 <viveksingh9301@gmail.com>
2024-01-11 19:51:06 +0530

    ref: refactor Cart component and store cart data in a separate list

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,16 +2,35 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 function Cart() {
-  const items = useSelector(store => store.cart.items);
+  const [cartItems, setCartItems] = useState([]);
   const [cartTotal, setCartTotal] = useState(0);
+
+  const items = useSelector(store => store.cart.items);
+
+  useEffect(() => {
+    populateCartItemsList();
+  }, []);
   useEffect(() => {
     calculateCartTotal();
-  }, [items]);
+  }, [cartItems]);
+
+  function populateCartItemsList() {
+    const itemsList = [];
+    for (const item of items) {
+      const { id, name, price } = item?.card?.info;
+      itemsList.push({
+        id,
+        name,
+        price: price / 100
+      });
+    }
+    setCartItems(itemsList);
+  }
 
   const calculateCartTotal = () => {
     let total = 0;
-    for (const item of items) {
-      total += item?.card?.info.price / 100;
+    for (const item of cartItems) {
+      total += item.price;
     }
     setCartTotal(total);
   };
@@ -21,13 +40,12 @@ function Cart() {
     <main className="w-[560px] m-4 mx-auto p-2">
       <h1 className="text-3xl">Cart</h1>
       <section className="my-5">
-        {items.map(item => {
-          const { id, name, price, defaultPrice } = item?.card?.info;
+        {cartItems.map(item => {
+          const { id, name, price } = item;
           return (
             <article key={id} className="w-100 p-2 px-3 mb-3 bg-gray-100 rounded-md flex justify-between items-center">
               <div className="flex flex-col">
-                <span className="text-gray">{name}</span>{' '}
-                <span className="text-lg font-semibold">&#8377; {price / 100 || defaultPrice / 100}</span>
+                <span className="text-gray">{name}</span> <span className="text-lg font-semibold">&#8377; {price}</span>
               </div>
             </article>
           );
