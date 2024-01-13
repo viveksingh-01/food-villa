de9e5c59a5f0e44b4d1e34e7ebf01e72ca6be1bb
viveksingh-01 <viveksingh9301@gmail.com>
2024-01-13 10:13:55 +0530

    feat: increment item-count in the cart on click of plus button
    - dispatch addItem action
    - invoke populateCartItemsList method

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem } from '../utils/cartSlice';
 
 function Cart() {
   const [cartItems, setCartItems] = useState([]);
   const [cartTotal, setCartTotal] = useState(0);
 
   const items = useSelector(store => store.cart.items);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     populateCartItemsList();
-  }, []);
+  }, [items]);
   useEffect(() => {
     calculateCartTotal();
   }, [cartItems]);
@@ -53,6 +55,11 @@ function Cart() {
     setCartTotal(total);
   };
 
+  function incrementItemCount(id) {
+    const item = items.find(e => e?.card?.info.id == id);
+    dispatch(addItem(item));
+  }
+
   if (items.length === 0) return null;
   return (
     <main className="w-[560px] m-4 mx-auto p-2">
@@ -65,12 +72,17 @@ function Cart() {
               key={id}
               className="w-100 p-2 px-3 mb-3 bg-gray-100 rounded-md flex justify-between items-center shadow-sm">
               <div className="flex flex-col">
-                <span className="text-gray">{name}</span> <span className="text-lg font-semibold">&#8377; {price}</span>
+                <span className="text-gray">{name}</span>
+                <span className="text-lg font-semibold">&#8377; {price}</span>
               </div>
               <div className="w-[96px] px-2 flex justify-between items-center rounded-md bg-green-500 text-white shadow-md text-lg font-semibold">
                 <span className="p-1 -mt-1 text-xl hover:cursor-pointer hover:text-2xl">-</span>
                 <span className="p-1">{count}</span>
-                <span className="p-1 -mt-1 text-xl hover:cursor-pointer hover:text-2xl">+</span>
+                <span
+                  className="p-1 -mt-1 text-xl hover:cursor-pointer hover:text-2xl"
+                  onClick={() => incrementItemCount(item.id)}>
+                  +
+                </span>
               </div>
             </article>
           );
